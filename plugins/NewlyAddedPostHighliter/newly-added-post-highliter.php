97b8247c097cc54450dd8f061db3e66285f67b22
Remigiusz Pisarski <remigiusz.pisarski@axxiomehealth.com>
2023-03-01 08:33:29 +0100

    Add local config for wordpress and folder for plugin

---
+++ plugins/NewlyAddedPostHighliter/newly-added-post-highliter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Newly Added Post Highliter
+* Plugin URI: https://example.com/plugins/Newly Added Post Highliter/
+* Description: Highlight newly posts with tag.
+* Version: 1.0
+* Requires at least: 5.0
+* Requires PHP: 7.2
+* Author: Remigiusz Pisarski
+* Author URI: https://darksource.pl/
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
